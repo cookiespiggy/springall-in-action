9786a8818db6b0e272f0c33710ce3e91d4eb3e49
mizhe <mizhe@meituan.com>
2023-09-15 18:06:38 +0800

    BeanFactoryPostProcessor学习

--- src/main/java/com/example/ext/Ext1.java
+++ src/main/java/com/example/ext/Ext1.java
@@ -1,5 +1,31 @@
 package com.example.ext;
 
-public class Ext1 {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
+public class Ext1 extends PropertySourcesPlaceholderConfigurer implements ApplicationContextAware, BeanFactoryAware {
+
+    private ApplicationContext applicationContext;
+
+    private BeanFactory beanFactory;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) {
+        super.setBeanFactory(beanFactory);
+        this.beanFactory = beanFactory;
+    }
+
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactoryToProcess) throws BeansException {
+        super.postProcessBeanFactory(beanFactoryToProcess);
+    }
 }
